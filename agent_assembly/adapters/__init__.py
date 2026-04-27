71b6f5dcbd00af6d103128a8454c2c7eeab6e0df
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 12:05:16 +0800

    ♻️ (exports): Export AdapterRegistry and AdapterInfo from adapters package

--- agent_assembly/adapters/__init__.py
+++ agent_assembly/adapters/__init__.py
@@ -1,3 +1,4 @@
 from agent_assembly.adapters.base import FrameworkAdapter, GovernanceInterceptor
+from agent_assembly.adapters.registry import AdapterInfo, AdapterRegistry
 
-__all__ = ["GovernanceInterceptor", "FrameworkAdapter"]
+__all__ = ["GovernanceInterceptor", "FrameworkAdapter", "AdapterInfo", "AdapterRegistry"]
