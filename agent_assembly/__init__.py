2aa3359a286b4963eab7c14487419183516411b0
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-29 22:31:14 +0800

    ✨ (core): implement init_assembly orchestrator with AssemblyContext

--- agent_assembly/__init__.py
+++ agent_assembly/__init__.py
@@ -1,7 +1,7 @@
 """Agent Assembly Python SDK."""
 
 from agent_assembly.adapters import FrameworkAdapter, GovernanceInterceptor
-from agent_assembly.core import init_assembly
+from agent_assembly.core import AssemblyContext, init_assembly
 from agent_assembly.exceptions import (
     AdapterValidationError,
     AgentError,
@@ -17,6 +17,7 @@ __version__ = "0.0.0"
 __all__ = [
     "__version__",
     "init_assembly",
+    "AssemblyContext",
     "GovernanceInterceptor",
     "FrameworkAdapter",
     "AssemblyError",
