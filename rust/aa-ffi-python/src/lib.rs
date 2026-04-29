12fddf1f1b400571fa3916b715b9a02d6eebce61
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-29 23:05:03 +0800

    ✨ (ffi): add internal tokio channel worker for runtime client

--- rust/aa-ffi-python/src/lib.rs
+++ rust/aa-ffi-python/src/lib.rs
@@ -2,7 +2,10 @@
 
 use once_cell::sync::Lazy;
 use pyo3::prelude::*;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use tokio::runtime::Runtime;
+use tokio::sync::mpsc;
 
 static TOKIO_RUNTIME: Lazy<Runtime> = Lazy::new(|| {
     tokio::runtime::Builder::new_multi_thread()
@@ -51,19 +54,47 @@ impl PolicyResult {
 struct RuntimeClient {
     #[pyo3(get)]
     socket_path: String,
+    sender: Option<mpsc::UnboundedSender<WorkerMessage>>,
+    closed: Arc<AtomicBool>,
+}
+
+#[derive(Clone)]
+enum WorkerMessage {
+    Event(GovernanceEvent),
+    Close,
 }
 
 #[pymethods]
 impl RuntimeClient {
     #[new]
     fn new(socket_path: String) -> Self {
-        Self { socket_path }
+        Self {
+            socket_path,
+            sender: None,
+            closed: Arc::new(AtomicBool::new(true)),
+        }
     }
 
     #[staticmethod]
     fn connect(socket_path: String) -> Self {
         let _ = &*TOKIO_RUNTIME;
-        Self { socket_path }
+        let (sender, mut receiver) = mpsc::unbounded_channel::<WorkerMessage>();
+        let closed = Arc::new(AtomicBool::new(false));
+        let closed_for_task = Arc::clone(&closed);
+        TOKIO_RUNTIME.spawn(async move {
+            while let Some(message) = receiver.recv().await {
+                match message {
+                    WorkerMessage::Event(_event) => {}
+                    WorkerMessage::Close => break,
+                }
+            }
+            closed_for_task.store(true, Ordering::SeqCst);
+        });
+        Self {
+            socket_path,
+            sender: Some(sender),
+            closed,
+        }
     }
 }
 
