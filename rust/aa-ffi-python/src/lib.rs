08bdd6a1a7e2ffc14057d0c7c2b3e727e1c374c2
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-29 23:03:52 +0800

    ✨ (ffi): add GovernanceEvent pyclass skeleton

--- rust/aa-ffi-python/src/lib.rs
+++ rust/aa-ffi-python/src/lib.rs
@@ -2,7 +2,23 @@
 
 use pyo3::prelude::*;
 
+#[pyclass(module = "agent_assembly._core")]
+#[derive(Clone)]
+struct GovernanceEvent {
+    #[pyo3(get)]
+    payload_json: String,
+}
+
+#[pymethods]
+impl GovernanceEvent {
+    #[new]
+    fn new(payload_json: String) -> Self {
+        Self { payload_json }
+    }
+}
+
 #[pymodule]
-fn _core(_py: Python<'_>, _module: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _core(_py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
+    module.add_class::<GovernanceEvent>()?;
     Ok(())
 }
